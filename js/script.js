7d8c5de79d4649b19185548e1433e3e24d2a3c1a
kholifah <tm.kholifah@gmail.com>
2016-02-12 09:31:38 +0700

    push all source

---
+++ js/script.js
@@ -0,0 +1,59 @@
+///*** --burger nav-menu ***/
+//	
+//	document.getElementById("burger-menu").style.display="none";
+//	function ReverseHamburgerMenuDisplay()
+//	{
+//	   var id = document.getElementById("burger-menu");
+//	   if( id.style.display == "none" ) { id.style.display = "block"; }
+//	   else { id.style.display = "none"; }
+//	}
+//	function HamburgerMenuItemOver(id) { id.style.backgroundColor = "#efefef"; }
+//	function HamburgerMenuItemOut(id) { id.style.backgroundColor = "#fff"; }
+	
+
+
+//burger
+var theToggle = document.getElementById('toggle');
+
+// based on Todd Motto functions
+// http://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
+
+
